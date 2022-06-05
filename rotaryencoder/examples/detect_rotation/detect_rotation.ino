bccb4fa3d39b65cfd775f12fc615d8f8378c1d6c
koheian <koheian11@gmail.com>
2022-06-05 15:23:23 +0900

    move example file

---
+++ rotaryencoder/examples/detect_rotation/detect_rotation.ino
@@ -0,0 +1,38 @@
+/* test of totary encoder
+ *  
+ *  Arduinoとの接続:
+ *  5V
+ *  GND
+ *  ロータリーエンコーダA相→digital 2 pin
+ *  ロータリーエンコーダB相→digital 3 pin
+ *  
+ *  used rotary encoder: https://akizukidenshi.com/catalog/g/gP-00292/
+ *  ref for program: http://219.117.208.26/~saka/ham/rotary_encoder/
+ *  ref for circuit: https://monorepi.jp/archives/1440
+ */
+#include "rotaryencoder.h"
+
+void setup() {
+  Serial.begin(9600);
+
+  attachInterrupt(0, when_interrupt, CHANGE); // pin2(a)
+  attachInterrupt(1, when_interrupt, CHANGE); // pin3(b)
+  
+}
+
+void loop() {
+  if (flag_interrupt == true) {
+    switch (rotate_direction) {
+      case d_still:
+        Serial.println("still");
+        break;
+      case d_right:
+        Serial.println("right");
+        break;
+      case d_left:
+        Serial.println("left");
+        break;
+    }
+    flag_interrupt = false;
+  }
+}
